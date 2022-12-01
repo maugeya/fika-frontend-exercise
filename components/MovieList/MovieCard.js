94d15bfb39c1797b9704255852e3f22769c1503c
Angela Maugey <angela.maugey@kinandcarta.com>
2022-12-01 17:14:42 +0000

    Update MovieCard to use useFetchMovies return data and display genres in Flatlist

--- components/MovieList/MovieCard.js
+++ components/MovieList/MovieCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList } from 'react-native';
+import uuid from 'react-native-uuid';
+import { TEST_IDS } from '../../utils/constants';
 
 const styles = StyleSheet.create({
   cardHeading: {
@@ -8,21 +10,28 @@ const styles = StyleSheet.create({
   },
   cardText: {
     color: '#f8f5f2',
+    flex: 1,
+    flexWrap: 'wrap',
   },
   image: {
-    height: 120,
-    width: 120,
+    height: 160,
+    width: 160,
     resizeMode: 'contain',
   },
   movieCardContainer: {
+    alignItems: 'center',
     padding: 10,
     borderRadius: 5,
     backgroundColor: '#232323',
     flexDirection: 'row',
+    flex: 1,
+  },
+  titleHeader: {
+    width: '70%',
   },
 });
 
-const MovieCard = ({ item }) => {
+function MovieCard({ item }) {
   return (
     <View key={item.id} style={styles.movieCardContainer}>
       <Image
@@ -30,20 +39,31 @@ const MovieCard = ({ item }) => {
           uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}`,
         }}
         style={styles.image}
-        testID='movie-image'
+        testID={TEST_IDS.MOVIE_CARD_IMAGE}
       />
       <View style={styles.cardTextContent}>
-        <View>
+        <View style={styles.titleHeader}>
           <Text style={styles.cardHeading}>Title</Text>
           <Text style={styles.cardText}>{item.title}</Text>
         </View>
         <View>
           <Text style={styles.cardHeading}>Genres</Text>
-          <Text style={styles.cardText}>Horror, pancakes, sand</Text>
+          <FlatList
+            data={item.genres}
+            listKey={() => uuid.v4()}
+            renderItem={({ item }) => (
+              <Text
+                style={styles.cardText}
+                testID={TEST_IDS.MOVIE_CARD_GENRE_ITEM}
+              >
+                {item}
+              </Text>
+            )}
+          />
         </View>
       </View>
     </View>
   );
-};
+}
 
 export { MovieCard };
--- components/MovieList/MovieCard.test.js
+++ components/MovieList/MovieCard.test.js
@@ -1,22 +1,28 @@
 import { render, screen } from '@testing-library/react-native';
-import { mockedAPIResponse } from '../../src/mocks/handlers';
+import { TEST_IDS } from '../../utils/constants';
 
 import { MovieCard } from './MovieCard';
+const mockMovieCardData = {
+  poster_path: '/some-path.png',
+  title: '28 days later',
+  genres: ['Horror', 'Fantasy'],
+};
 
 describe('/components/MovieList/MovieCard', () => {
   describe('when rendered with a movie as the `item` prop', () => {
     it('should display the MovieCard component and all expected parts', () => {
-      render(<MovieCard item={mockedAPIResponse.results[0]} />);
+      render(<MovieCard item={mockMovieCardData} />);
 
       const titleHeader = screen.getByText('Title');
       const movieTitle = screen.getByText('28 days later');
       const genreHeader = screen.getByText('Genres');
-
-      const image = screen.getByTestId('movie-image');
+      const genreTexts = screen.getAllByTestId(TEST_IDS.MOVIE_CARD_GENRE_ITEM);
+      const image = screen.getByTestId(TEST_IDS.MOVIE_CARD_IMAGE);
 
       expect(titleHeader).toBeDefined();
       expect(movieTitle).toBeDefined();
       expect(genreHeader).toBeDefined();
+      expect(genreTexts).toHaveLength(2);
       expect(image).toBeDefined();
     });
   });
