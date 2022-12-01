ba46181c1923bd7c5b64b0758f4ac58437819512
Angela Maugey <angela.maugey@kinandcarta.com>
2022-12-01 17:16:54 +0000

    Create constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,18 @@
 export const TEST_IDS = {
   MOVIE_LIST_CONTAINER: 'movie-list-container',
+  SEARCH_BAR_CONTAINER: 'search-bar-container',
+  SEARCH_BAR_INPUT: 'search-bar-input',
+  SEARCH_BAR_SUBMIT: 'search-bar-submit',
+  PAGE_SELECT_CONTAINER: 'page-select-container',
+  MOVIE_CARD_IMAGE: 'movie-card-image',
+  MOVIE_CARD_GENRE_ITEM: 'movie-card-genre-item',
 };
+
+export const BASE_SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=d432b933ecc6d5642d8d2befbc40c7ac&language=en-US&include_adult=false';
+
+export const BASE_URL =
+  'https://api.themoviedb.org/3/discover/movie?api_key=d432b933ecc6d5642d8d2befbc40c7ac&language=en-US&include_adult=false&page=';
+
+export const GENRES_URL =
+  'https://api.themoviedb.org/3/genre/movie/list?api_key=d432b933ecc6d5642d8d2befbc40c7ac&language=en-US';
